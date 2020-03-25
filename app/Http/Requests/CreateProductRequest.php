c9d131511c2f532da12c5be2af5222c461a87700
armzem <zarmundas@gmail.com>
2020-03-25 11:52:37 +0200

    Update product creation request

--- app/Http/Requests/CreateProductRequest.php
+++ app/Http/Requests/CreateProductRequest.php
@@ -24,6 +24,7 @@ class CreateProductRequest extends FormRequest
     public function rules()
     {
         return [
+            'category_id' => 'required',
             'name' => 'required|min:5',
         ];
     }
